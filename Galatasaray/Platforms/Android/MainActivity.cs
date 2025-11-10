291be882a48898095ac7de93f73695a535999c14
52558 <52558@NITRO-V15-JOHAN>
2025-11-10 16:55:08 -0600

    Agregar archivos de proyecto.

---
+++ Galatasaray/Platforms/Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace Galatasaray
+{
+    [Activity(
+        Theme = "@style/Maui.SplashTheme",
+        MainLauncher = true,
+        LaunchMode = LaunchMode.SingleTop,
+        ScreenOrientation = ScreenOrientation.FullSensor, // <— permite portrait/landscape según el sensor
+        ConfigurationChanges = ConfigChanges.ScreenSize
+                             | ConfigChanges.Orientation
+                             | ConfigChanges.UiMode
+                             | ConfigChanges.ScreenLayout
+                             | ConfigChanges.SmallestScreenSize
+                             | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+    }
+}
